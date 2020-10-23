7d743e65b15d50b08a5146ce74e1f92c6c6e55e0
Syed Mohammad Quadri <55687431+SMQuadri@users.noreply.github.com>
2020-10-24 00:14:15 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@ import firebase from 'firebase';
 import 'firebase/firestore';
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyA2uivjDQJgQ4wHyGtpwftAD8YoUN6GoHA",
-  authDomain: "netflix-10001.firebaseapp.com",
-  databaseURL: "https://netflix-10001.firebaseio.com",
-  projectId: "netflix-10001",
-  storageBucket: "netflix-10001.appspot.com",
-  messagingSenderId: "891883150684",
-  appId: "1:891883150684:web:ae51518c7131858a51b599",
-  measurementId: "G-B93DD7FYPQ"
+  apiKey: //Paste your firebase config keys here,
+  authDomain: //Paste your firebase config keys here,
+  databaseURL: //Paste your firebase config keys here,
+  projectId: //Paste your firebase config keys here,
+  storageBucket: //Paste your firebase config keys here,
+  messagingSenderId: //Paste your firebase config keys here,
+  appId: //Paste your firebase config config keys here,
+  measurementId: //Paste your firebase config keys here
 });
 
 const auth = firebase.auth();
